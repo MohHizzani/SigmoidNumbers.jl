336b571b678c1a8d9b6dbd2098bf9786658c6120
Mohammad Hizzani <moh.hizzani@gmail.com>
2019-11-14 16:50:05 +0200

    uncomment some includes

--- src/SigmoidNumbers.jl
+++ src/SigmoidNumbers.jl
@@ -23,18 +23,18 @@ module SigmoidNumbers
   # include("Tools/fft.jl")
   # include("Tools/ubox.jl")
 
-  # include("quire.jl")
-  # include("fdp-matrixsolve.jl")
-  # include("quire_solve.jl")
+  include("quire.jl")
+  include("fdp-matrixsolve.jl")
+  include("quire_solve.jl")
 
-  # #patching julia's native lu factorization.
-  # include("LUpatches.jl")
+  #patching julia's native lu factorization.
+  include("LUpatches.jl")
 
-  # #including blas functions
-  # include("BLAS/blas.jl")
-  # include("BLAS/Level1.jl")
-  # include("BLAS/Level2-er.jl")
-  # include("BLAS/Level2-mv.jl")
-  # include("BLAS/Level2-sv.jl")
+  #including blas functions
+  include("BLAS/blas.jl")
+  include("BLAS/Level1.jl")
+  include("BLAS/Level2-er.jl")
+  include("BLAS/Level2-mv.jl")
+  include("BLAS/Level2-sv.jl")
 
 end # module
