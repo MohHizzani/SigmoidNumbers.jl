640ffc635a33ca9ebf6582d2fde9a18f2b68661b
Mohammad Hizzani <moh.hizzani@gmail.com>
2019-11-14 21:18:20 +0200

    add missing (

--- src/Posits/convert.jl
+++ src/Posits/convert.jl
@@ -3,7 +3,7 @@
 import convert
 
 Base.convert(::Type{Posit{N,0}}, bval::Bool) where {N} = reinterpret(Posit{N,0}, bval * (@invertbit))
-function Base.convert::Type{Bool}, pval::Posit{N,0}) where {N}
+function Base.convert(::Type{Bool}, pval::Posit{N,0}) where {N}
   (@s(pval) < 0) && throw(InexactError())
   (@u(pval) > 0x4000_0000_0000_0000) && throw(InexactError())
   return @u(pval) > 0x2000_0000_0000_0000
